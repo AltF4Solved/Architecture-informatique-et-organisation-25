53b439d1f3dc9a7ea48c4e06ca1422462bcbccb4
AltF4Solved <163351316+AltF4Solved@users.noreply.github.com>
2025-02-12 13:57:23 +0100

    Create register.cpp

---
+++ Lab 2/Final/register.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include "registers.h"
+
+using namespace std;
+
+
+Registers::Registers ()
+{
+    for(int i = 0; i < 32; i++)
+    {
+        rgs[i] = 0;
+    }
+    pcCount = 0;
+}
+
+void Registers::setRegister(int regNum, int value)
+{
+    if (regNum == 0)
+    {
+        rgs[regNum] = 0;
+    }
+    else
+    {
+        rgs[regNum] = value;
+    }
+}
+
+int Registers::getRegister(int regNum)
+{
+    return rgs[regNum];
+}
+
+void Registers::setPC(int value)
+{
+    pcCount = value;
+}
+
+int Registers::getPC()
+{
+    return pcCount;
+}
+
+void Registers::print()
+{
+    for(int i = 0; i < 32; i++){
+        cout << "Value Register : " << i << "\t" << rgs[i] << "\n";
+    }
+    cout << "Program Count :\t" << pcCount << "\n";
+
+}
